cab2c728184e5a3393c7b79f6fb5bcfa2abf6d41
Jake Archibald <jaffathecake@gmail.com>
2026-03-30 12:31:31 +0100

    Chrome transition hack

---
+++ public/gross-cube-transition-shaded-chrome-hack/script.js
@@ -0,0 +1,31 @@
+import { getPageContent, onLinkNavigate, transitionHelper } from "../utils.js";
+
+let closeWatcher = null;
+
+function tryCloseWatcherHack() {
+  if (navigator.userAgentData?.platform !== "Android") return;
+  if (closeWatcher) return;
+
+  closeWatcher = new CloseWatcher();
+  closeWatcher.onclose = () => {
+    closeWatcher = null;
+    history.back();
+  };
+}
+
+onLinkNavigate(async ({ toPath, isBack, hasUAVisualTransition }) => {
+  const content = await getPageContent(toPath);
+
+  const transition = transitionHelper({
+    skipTransition: hasUAVisualTransition,
+    types: isBack ? ["back"] : [],
+    updateDOM() {
+      // This is a pretty heavy-handed way to update page content.
+      // In production, you'd likely be modifying DOM elements directly,
+      // or using a framework.
+      // innerHTML is used here just to keep the DOM update super simple.
+      document.body.innerHTML = content;
+      tryCloseWatcherHack();
+    },
+  });
+});
